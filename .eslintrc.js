cd03c2816212d254e7c5c7f6145470bbdfb40a7f
TheMythologist <leekaixuan2001@gmail.com>
2026-03-07 13:45:52 -0800

    style: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,18 @@ module.exports = {
   rules: {
     'prettier/prettier': 'warn',
     'import/order': ['warn', { alphabetize: { order: 'asc' } }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
   },
   settings: {
     'import/resolver': {
