32f5f2112ce84299c7f989e7d6b08a3b65fd16aa
TheMythologist <leekaixuan2001@gmail.com>
2026-03-07 18:54:05 -0800

    build: remove unused deps

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,16 @@
 import { Ionicons } from '@expo/vector-icons';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import * as Clipboard from 'expo-clipboard';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Linking, StyleSheet, View, Text, TouchableOpacity, useColorScheme } from 'react-native';
 import Toast from 'react-native-toast-message';
 import { CurrentUrlContext } from '@/hooks/useCurrentUrlContext';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
   const [currentUrl, setCurrentUrl] = useState('https://freedium-mirror.cfd/');
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   const copyToClipboard = async () => {
     await Clipboard.setStringAsync(currentUrl);
